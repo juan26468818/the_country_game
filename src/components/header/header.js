96127d14f9a62b55455ee5bc81cd045a65c994b7
Juan Ayala <juanayala099@gmail.com>
2022-09-05 21:34:13 -0300

    add audio img and fix audio bug on click

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,24 @@
 import "./header.css"
 import { useState } from "react";
+import audio from "../../audio.png"
+import noAudio from "../../no-audio.png"
 
 const Header = (()=>{
     const [volume, setVolume] = useState(1);
+    const [isAudioImg, setIsAudioImg] = useState(localStorage.getItem("Volume"));
+    const [audioImg, setAudioImg] = useState(localStorage.getItem("Volume") == 0 ? noAudio : audio);
     const handleVolume = (e =>{
         e.preventDefault()
         volume === 0 ? setVolume(1) : setVolume(0)
         localStorage.setItem("Volume", volume)
+        isAudioImg == 1 ? setIsAudioImg(0) : setIsAudioImg(1)
+        console.log(isAudioImg)
+        isAudioImg == 1 ? setAudioImg(audio) : setAudioImg(noAudio)
     })
     return (
         <header>
             <h1>Guess who has more people</h1>
-            <button type="button" className="volume-button" onClick={handleVolume}>Volume</button>
+            <img className="volume-button" onClick={handleVolume} src={audioImg}></img>
         </header>
     )
 })
